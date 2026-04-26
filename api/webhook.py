4d8ba1be7f1c99ce750dd87f25aa85ef8139d0a5
leangtaingmeng-sudo <leangtaingmeng@gmail.com>
2026-04-26 11:45:30 +0700

    Update webhook.py

--- api/webhook.py
+++ api/webhook.py
@@ -1,43 +1,46 @@
 from fastapi import FastAPI, Request
 import os
 from telegram import Update, Bot
-
+from dataclasses import dataclass, field
+from typing import Any
+from  bot import Agent
 app = FastAPI()
 TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
 
-def handle_response(text: str) -> str:
+bot = Bot(token=TOKEN)
+agent = Agent(model="qwen2.5-coder-0.5b-instruct-f1")
+ai_mode = False
+def handle_response(text:str)->str:
     if text.lower() == 'hi':
         return "hi from da web"
-    return "fuq u nigger"
-@app.get("/api/webhook")
-async def check_health():
-    return {"status": "The bot is awake and listening!"}
+    return "fuck u nigger"
+    if ai_mode == True:
+        return agent.chat(text)
+def ai_mode_command():
+    ai_mode = not ai_mode
 @app.post("/api/webhook")
 async def webhook_handler(request: Request):
     # Parse the incoming JSON from Telegram
     data = await request.json()
     
-    # CRITICAL FIX: Open the bot connection ONLY when handling the request
-    async with Bot(token=TOKEN) as bot:
-        # Convert the JSON into a python-telegram-bot Update object
-        update = Update.de_json(data, bot)
-        
-        # Handle the message
-        if update.message and update.message.text:
-            user_text = update.message.text
-            chat_id = update.message.chat_id
-            
-            # --- YOUR AI LOGIC GOES HERE ---
+    # Convert the JSON into a python-telegram-bot Update object
+    update = Update.de_json(data, bot)
+    
+    # 3. Handle the message
+    if update.message and update.message.text:
+        user_text = update.message.text
+        chat_id = update.message.chat_id
+        if user_text.startswith("/start"):
+            await update.message.reply_text("Hello digger, i am a very smart telegram Bot")
+        elif user_text.startswith("/help"):
+            await update.message.reply_text("Bigger, I am here to help")
+        elif user_text.startswith("/ai"):
+            ai_mode_command()
+            await update.message.reply_text("rigger, you can now chat with an ai")        
+        # --- YOUR AI LOGIC GOES HERE ---
+        else:
             reply = handle_response(user_text)
-            
-            # Send the response back asynchronously 
-            await bot.send_message(
-                chat_id=chat_id, 
-                text=f"The AI is processing: {user_text}"
-            )
-            
-            # Send the actual generated reply
             await update.message.reply_text(reply)
-            
+        
     # Always return a 200 OK so Telegram knows the message was received
     return {"ok": True}
